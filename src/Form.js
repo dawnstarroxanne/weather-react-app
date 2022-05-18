1049a919cf8e0aa2aefa4017c5b210142efe6632
dawnstarroxanne <99957190+dawnstarroxanne@users.noreply.github.com>
2022-05-18 12:12:13 +0100

    organised the structure in weather and updated css

--- src/Form.js
+++ src/Form.js
@@ -3,19 +3,19 @@ import React from "react";
 export default function Form() {
   return (
     <div className="Form">
-      <form id="search-form" class="mb-4">
-        <div class="row">
-          <div class="col-9">
+      <form id="search-form" className="mb-4">
+        <div className="row">
+          <div className="col-9">
             <input
               type="search"
               placeholder="enter city"
-              class="form-control"
+              className="form-control"
               id="city-input"
               autocomplete="off"
             />
           </div>
-          <div class="col-3">
-            <input type="submit" value="search" class="btn btn-primary" />
+          <div className="col-3">
+            <input type="submit" value="search" className="btn btn-primary" />
           </div>
         </div>
       </form>
--- src/Weather.js
+++ src/Weather.js
@@ -1,17 +1,27 @@
 import React from "react";
+import "./Weather.css";
 
 export default function Weather() {
   return (
     <div className="Weather">
-      <h3>Partly Cloudy</h3>
+      <h1>Lisbon</h1>
       <ul>
-        <li>
-          Humidity: 60<span id="humidity"></span>%
-        </li>
-        <li>
-          Wind: 14<span id="wind"></span>km/h
-        </li>
+        <li>wednesday 15:00</li>
+        <li>Partly Cloudy</li>
       </ul>
+      <div className="row">
+        <div className="col-6">25ºC</div>
+        <div className="col-6">
+          <ul>
+            <li>
+              Humidity: 60<span id="humidity"></span>%
+            </li>
+            <li>
+              Wind: 14<span id="wind"></span>km/h
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
