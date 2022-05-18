ceb80199e48016e5e0a484ff0c8fae9704f27b28
dawnstarroxanne <99957190+dawnstarroxanne@users.noreply.github.com>
2022-05-18 14:48:03 +0100

    removed components, updated css

--- src/App.js
+++ src/App.js
@@ -2,24 +2,14 @@ import "./App.css";
 import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
 
-import Form from "./Form";
-import City from "./City";
-import Date from "./Date";
-import Temp from "./Temp";
 import Weather from "./Weather";
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Form />
-        <City />
-        <Date />
-        <Temp />
-        <div class="col-6">
-          <Weather />
-        </div>
+        <Weather />
         <footer>
-          Coded by
+          Coded by -
           <a
             href="https://github.com/dawnstarroxanne/weather-react-app"
             target="_blank"
--- src/Weather.js
+++ src/Weather.js
@@ -4,7 +4,33 @@ import "./Weather.css";
 export default function Weather() {
   return (
     <div className="Weather">
+      <div className="Form">
+        <form id="search-form" className="mb-4">
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="enter city"
+                className="form-control"
+                id="city-input"
+                autocomplete="off"
+                autoFocus="on"
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="search"
+                className="btn btn-primary w-100"
+              />
+            </div>
+          </div>
+        </form>
+      </div>
       <h1>Lisbon</h1>
+      <div className="Date">
+        last updated: 15:30 <span id="date">Fri 22 April</span>
+      </div>
       <ul>
         <li>wednesday 15:00</li>
         <li>Partly Cloudy</li>
