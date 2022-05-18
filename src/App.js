7a4466c5d87b4458670c148ea6bab290cc30aa63
dawnstarroxanne <99957190+dawnstarroxanne@users.noreply.github.com>
2022-05-18 10:39:32 +0100

    added bootstrap and did npm update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
 
 import Form from "./Form";
 import City from "./City";
@@ -9,23 +10,25 @@ import Weather from "./Weather";
 export default function App() {
   return (
     <div className="App">
-      <Form />
-      <City />
-      <Date />
-      <Temp />
-      <div class="col-6">
-        <Weather />
+      <div className="container">
+        <Form />
+        <City />
+        <Date />
+        <Temp />
+        <div class="col-6">
+          <Weather />
+        </div>
+        <footer>
+          Coded by
+          <a
+            href="https://github.com/dawnstarroxanne/weather-react-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Dawn Roxanne
+          </a>
+        </footer>
       </div>
-      <footer>
-        Coded by
-        <a
-          href="https://github.com/dawnstarroxanne/weather-react-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Dawn Roxanne
-        </a>
-      </footer>
     </div>
   );
 }
